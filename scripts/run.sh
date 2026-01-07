4934cc14df314e3ca34ee1192083f7c31213e488
nirvana <bbaranoff>
2026-01-08 00:18:26 +0100

    commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -28,14 +28,14 @@ sleep 1
 #################################
 # Fenêtre 0 : FakeTRX
 #################################
-tmux new-session -d -s "$SESSION" -n srsepc
+tmux new-session -d -s "$SESSION" -n CORE LTE/NSA
 tmux send-keys -t "$SESSION:0" "srsepc" C-m
 sleep 2
 
 #################################
 # Fenêtre 1 : MS1 (trxcon + mobile)
 #################################
-tmux new-window -t "$SESSION:1" -n srsenb
+tmux new-window -t "$SESSION:1" -n TRX LTE/NSA
 tmux send-keys -t "$SESSION:1" "srsenb" C-m
 sleep 2
 
@@ -44,6 +44,18 @@ sleep 2
 #################################
 tmux new-window -t "$SESSION:2" -n asterisk
 tmux send-keys -t "$SESSION:2" "asterisk -rvvv" C-m
+
+#################################
+# Fenêtre 2 : Asterisk CLI
+#################################
+tmux new-window -t "$SESSION:3" -n CORE EDGE
+tmux send-keys -t "$SESSION:3" "/etc/osmocom/status.sh" C-m
+
+#################################
+# Fenêtre 2 : Asterisk CLI
+#################################
+tmux new-window -t "$SESSION:3" -n TRX EDGE
+tmux send-keys -t "$SESSION:3" "osmo-trx-uhd" C-m
 
 #################################
 # Final
